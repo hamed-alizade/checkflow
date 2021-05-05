7b1aafd9ac66de354a629dd9fe5c45b750a325ab
Hamed Alizade <mohamad.talkhaby@gmail.com>
2021-05-05 15:17:21 +0430

    refactor: add after argument in addState method and renamegetStateKey to isExist

--- app/Services/Flow.php
+++ app/Services/Flow.php
@@ -19,9 +19,32 @@ class Flow
         return $this->name;
     }
 
-    public function addState(State $state)
+    public function addState(State $state, State $after = null)
     {
-        $this->flow[$state->getName()] = $state;
+        if($after) {
+            $afterOrder = $this->isExist($after->getName());
+            if ($afterOrder >= 0) {
+                $firstSlice = array_slice($this->flow, 0, $afterOrder + 1);
+                $secondSlice = array_slice($this->flow, $afterOrder + 1, count($this->flow));
+                $this->flow = $firstSlice;
+                $this->flow[$state->getName()] = $state;
+                $this->flow = array_merge($this->flow, $secondSlice);
+            }
+            else {
+                $this->flow[$state->getName()] = $state;
+            }
+        }
+        else {
+            $this->flow[$state->getName()] = $state;
+        }
+    }
+
+    public function removeState(State $state)
+    {
+        $stateKey = $this->isExist($state->getName());
+        if ($stateKey >= 0) {
+            unset($this->flow[$state->getName()]);
+        }
     }
 
     public function getFlow(): array
@@ -37,17 +60,17 @@ class Flow
         }
     }
 
-    private function getStateKey(string $stateName)
+    private function isExist(string $stateName) : int
     {
         $flowKey = array_keys($this->flow);
         $stateKey = array_search($stateName, $flowKey);
-        if($stateKey < 0 or $stateKey == false) { return -1; }
+        if($stateKey < 0 or $stateKey === false) { return -1; }
         return $stateKey;
     }
 
     public function getNextState(string $currentStateName): string
     {
-        $currentStateKey = $this->getStateKey($currentStateName);
+        $currentStateKey = $this->isExist($currentStateName);
         if($currentStateKey < 0) { abort(404); }
         $currentState = $this->flow[$currentStateName];
         $next = $currentState->getNext();
