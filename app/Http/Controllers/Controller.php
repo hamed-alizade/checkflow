f711dd319377deea97a0c1d473f56597ad986908
Hamed Alizade <mohamad.talkhaby@gmail.com>
2021-05-05 15:22:52 +0430

    refactor: move flow define process to Controller construct method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Operations\CheckCardPaymentStatus;
+use App\Operations\DietTypePermission;
+use App\Operations\EndPaymentProcess;
+use App\Operations\SicknessStatus;
+use App\Operations\StartPayment;
+use App\Services\Flow;
+use App\Services\State;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +17,54 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $reg;
+    public function __construct(){
+        $this->reg = new Flow('reg');
+        $dietType = new State('diet/type');
+        $size = new State('size');
+        $report = new State('report');
+        $sickSelect = new State('sick/select');
+        $package = new State('package');
+        $dietTypePermission = new DietTypePermission();
+        $sicknessStatus = new SicknessStatus();
+        $startPayment = new StartPayment();
+        $package->setOperation($dietTypePermission);
+        $package->setOperation($sicknessStatus);
+        $package->setOperation($startPayment);
+
+        $payment = new Flow('payment');
+        $bill = new State('bill');
+        $card = new State('card');
+        $cardWait = new State('card/wait');
+        $cardConfirm = new State('card/confirm');
+        $cardReject = new State('card/reject');
+        $checkCardPaymentStatus = new CheckCardPaymentStatus();
+        $endPaymentProcess = new EndPaymentProcess();
+        $cardWait->setOperation($checkCardPaymentStatus);
+        $cardConfirm->setOperation($endPaymentProcess);
+        $cardReject->setOperation($endPaymentProcess);
+
+        $payment->addState($bill);
+        $payment->addState($card);
+        $payment->addState($cardWait);
+        $payment->addState($cardConfirm);
+        $payment->addState($cardReject);
+
+        $this->reg->addState($dietType);
+        $this->reg->addState($size);
+        $this->reg->addState($report);
+        $this->reg->addState($package);
+        $this->reg->addState($sickSelect, $report);
+
+        $test=new Flow('test');
+        $type = new State('test_type');
+        $type2 = new State('test_type2');
+        $test->addState($type);
+        $test->addState($type2);
+
+        $payment->addAccessory($test);
+
+        $this->reg->addAccessory($payment);
+    }
 }
--- app/Http/Controllers/flowController.php
+++ app/Http/Controllers/flowController.php
@@ -2,68 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Operations\CheckCardPaymentStatus;
-use App\Operations\DietTypePermission;
-use App\Operations\EndPaymentProcess;
-use App\Operations\SicknessStatus;
-use App\Operations\StartPayment;
-use App\Services\Flow;
-use App\Services\Operation;
-use App\Services\State;
 use Illuminate\Http\Request;
 
 class flowController extends Controller
 {
-    public function getNext(request $request)
+    public function getNext(request $request) : string
     {
-        $reg = new Flow('reg');
-        $dietType = new State('diet/type');
-        $size = new State('size');
-        $report = new State('report');
-        $sickSelect = new State('sick/select');
-        $package = new State('package');
-        $dietTypePermission = new DietTypePermission();
-        $sicknessStatus = new SicknessStatus();
-        $startPayment = new StartPayment();
-        $package->setOperation($dietTypePermission);
-        $package->setOperation($sicknessStatus);
-        $package->setOperation($startPayment);
-
-        $payment = new Flow('payment');
-        $bill = new State('bill');
-        $card = new State('card');
-        $cardWait = new State('card/wait');
-        $cardConfirm = new State('card/confirm');
-        $cardReject = new State('card/reject');
-        $checkCardPaymentStatus = new CheckCardPaymentStatus();
-        $endPaymentProcess = new EndPaymentProcess();
-        $cardWait->setOperation($checkCardPaymentStatus);
-        $cardConfirm->setOperation($endPaymentProcess);
-        $cardReject->setOperation($endPaymentProcess);
-
-        $payment->addState($bill);
-        $payment->addState($card);
-        $payment->addState($cardWait);
-        $payment->addState($cardConfirm);
-        $payment->addState($cardReject);
-
-        $reg->addState($dietType);
-        $reg->addState($size);
-        $reg->addState($report);
-        $reg->addState($sickSelect);
-        $reg->addState($package);
-
-        $test=new Flow('test');
-        $type = new State('test_type');
-        $type2 = new State('test_type2');
-        $test->addState($type);
-        $test->addState($type2);
-        
-        $payment->addAccessory($test);
-
-        $reg->addAccessory($payment);
-
-//        dd($reg->getFlow());
-        return $reg->getNextState($request['flow']);
+        return $this->reg->getNextState($request['flow']);
     }
 }
